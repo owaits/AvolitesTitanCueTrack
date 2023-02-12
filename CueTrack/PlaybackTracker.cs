d03ba0164e122350eb4d07902186f1634db7e32c
Oliver Waits <owaits@gmail.com>
2023-02-12 21:51:40 +0000

    Add project files.

---
+++ CueTrack/PlaybackTracker.cs
@@ -0,0 +1,76 @@
+﻿using LXProtocols.AvolitesWebAPI;
+using LXProtocols.AvolitesWebAPI.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CueTrack
+{
+    /// <summary>
+    /// Keeps track of an individual playback ensuring the load state and current cue stay in sync.
+    /// </summary>
+    public class PlaybackTracker
+    {
+        /// <summary>
+        /// Gets or sets the ID of the cue list being synced between the main and backup consoles.
+        /// </summary>
+        public int TitanId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the legend of the playback being tracked.
+        /// </summary>
+        public string? Legend { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current load state for the playback.
+        /// </summary>
+        public bool Loaded { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current cue ID for the cue list.
+        /// </summary>
+        public int CueId { get; set; }
+
+        /// <summary>
+        /// Called periodically to carry out the sync and update the state.
+        /// </summary>
+        /// <param name="handle">The most recent handle information for the playback.</param>
+        /// <param name="backup">The backup connection to sync to.</param>
+        public async Task Pulse(HandleInformation handle, Titan backup)
+        {
+            bool loaded = handle.Active;
+            if(Loaded != loaded)
+            {
+                if(loaded)
+                {
+                    await backup.Playbacks.Fire(HandleReference.FromTitanId(TitanId));
+                    Console.WriteLine($"{Legend} LOADED");
+                }
+                else
+                {
+                    await backup.Playbacks.Kill (HandleReference.FromTitanId(TitanId));
+                    Console.WriteLine($"{Legend} KILLED");
+                }
+                
+                Loaded = loaded;
+            }
+
+            if(Loaded)
+            {
+                int? cueId = handle.Information[1]["LiveCue"]?.GetValue<int>();
+                if(cueId != null && CueId != cueId)
+                {
+                    var cueInformation = await backup.CueLists.GetCue(TitanId, (int) cueId);
+
+                    await backup.CueLists.SetNextCue(HandleReference.FromTitanId(TitanId), cueInformation.CueNumber);
+                    await backup.CueLists.Play(HandleReference.FromTitanId(TitanId));
+                    CueId = (int) cueId;
+
+                    Console.WriteLine($"{Legend}:{cueInformation.Legend} CUE {cueInformation.CueNumber}");
+                }
+            }
+        }
+    }
+}
---
+++ CueTrack/Program.cs
@@ -0,0 +1,90 @@
+﻿
+
+using CueTrack;
+using LXProtocols.AvolitesWebAPI;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.Net;
+
+Console.WriteLine("AVOLITES TITAN CUE TRACK");
+Console.WriteLine("Synchronises the live cue between all running cue lists allowing a backup console to track the master conesole.");
+Console.WriteLine();
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddCommandLine(Environment.GetCommandLineArgs())
+    .Build();
+
+string? masterIP = config["master"];
+if (string.IsNullOrEmpty(masterIP))
+{
+    Console.WriteLine("Please enter the IP address of the MASTER console:");
+    masterIP = Console.ReadLine();
+    Console.WriteLine();
+}
+
+
+if (!IPAddress.TryParse(masterIP, out IPAddress? masterAddress))
+{
+    Console.WriteLine("Invalid MASTER IP address!");
+    Console.ReadLine();
+    return;
+}
+
+string? backupIP = config["backup"];
+if (string.IsNullOrEmpty(backupIP))
+{
+    Console.WriteLine("Please enter the IP address of the BACKUP console:");
+    backupIP = Console.ReadLine();
+    Console.WriteLine();
+}
+
+
+if (!IPAddress.TryParse(backupIP, out IPAddress? backupAddress))
+{
+    Console.WriteLine("Invalid BACKUP IP address!");
+    Console.ReadLine();
+    return;
+}
+
+int port = int.Parse(config["port"] ?? "4430");
+
+Titan master = new Titan(masterAddress, port);
+Titan backup = new Titan(backupAddress, port);
+
+while(!await master.IsConnected() || !await backup.IsConnected())
+{
+    Console.WriteLine("Waiting for connection to MASTER and BACKUP console...");
+    await Task.Delay(30000);
+}
+
+Console.WriteLine($"Started syncing {master.ConnectedDevice.Legend} to backup {backup.ConnectedDevice.Legend}");
+
+var trackers = new List<PlaybackTracker>();
+var handles = await master.Handles.GetHandles("Playbacks");
+
+foreach (var handle in handles.Where(item => item.Type == "cueListHandle"))
+{
+    trackers.Add(new PlaybackTracker()
+    {
+        TitanId = handle.TitanId,
+        Legend = handle.Legend
+    });
+}
+
+while (true)
+{
+    var handleUpdates = await master.Handles.GetHandles("Playbacks", verbose: true);
+    foreach(var tracker in trackers)
+    {
+        var handle = handleUpdates.FirstOrDefault(item=> item.TitanId == tracker.TitanId);
+        if(handle != null)
+        {
+            await tracker.Pulse(handle, backup);
+        }        
+    }
+    await Task.Delay(1000);
+}
+
+
+
